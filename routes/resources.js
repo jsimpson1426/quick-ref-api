f9be5658583afed1ac094c1cc0654e5d0486baf1
Simpson <joshuasimpson3@usicllc.com>
2021-08-02 13:24:24 -0400

    Added routes to resources.js

--- routes/resources.js
+++ routes/resources.js
@@ -17,7 +17,47 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', async (req,res) => {
   const {error} = validate(req.body);
-  if(error) return res.send(error.message[0]);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  try{
+    const resource = new Resource({
+      title: req.body.title,
+      description: req.body.description,
+      filename: req.body.filename,
+      tags: [...req.body.tags]
+    });
+
+    const result = await resource.save();
+    res.send(result);
+
+  } catch (err){
+    res.status(400).send(err.message);
+  }
+
+});
+
+router.put('/:id', async (req, res) => {
+  const { error } = validate(req.body); 
+  if (error) return res.status(400).send(error.details[0].message);
+
+  try{
+    const resource = await Resource.findByIdAndUpdate(req.params.id,
+      { 
+        title: req.body.title,
+        description: req.body.description,
+        filename: req.body.filename,
+        tags: [...req.body.tags]
+      }, 
+      { new: true }
+    );
+
+    if (!customer) return res.status(404).send('Resource not found.');
+
+    res.send(result);
+
+  } catch (err){
+    res.status(400).send(err.message);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
